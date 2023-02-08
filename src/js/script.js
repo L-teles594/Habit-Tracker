57f773845dde855233e45a3d0735d91e7355354f
L-teles594 <lteles350@gmail.com>
2023-02-07 21:49:48 -0300

    finalizado o projeto com salvamento dos dados no localStorage do navegador.

--- src/js/script.js
+++ src/js/script.js
@@ -1,14 +1,28 @@
 const form = document.querySelector("#form-habits");
 const nlwSetup = new NLWSetup(form);
+const button = document.querySelector('header button');
 
-const data = {
-    run: ['01-01', '01-02', , '01-03', '01-04', '01-05'],
-    water: ['01-01', '01-02'],
-    workout: ['01-01', '01-02'],
-    sleep: ['01-01', '01-02'],
-    dogWalk: ['01-01', '01-02'],
-    eatWell: ['01-01', '01-02']
+button.addEventListener('click', add);
+form.addEventListener('change', save);
+
+function add() {
+    const today = new Date().toLocaleDateString('pt-br').slice(0, -5);
+    const dayExists = nlwSetup.dayExists(today);
+
+    if (dayExists) {
+        alert('Dia já incluso 🔴');
+        return;
+    }
+
+    alert('Dia adicionado com sucesso! ✅')
+    nlwSetup.addDay(today);
 }
+
+function save() {
+    localStorage.setItem('NLWSetup@habits', JSON.stringify(nlwSetup.data));
+}
+
+const data = JSON.parse(localStorage.getItem('NLWSetup@habits')) || {};
 
 nlwSetup.setData(data)
 nlwSetup.load()
